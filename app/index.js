c850a4c8c6f99d623b7e74ddafdda4d806d76a7a
Alexey <k.alexey@btc-s.ru>
2019-08-09 17:57:24 +0300

    add some stuff

--- app/index.js
+++ app/index.js
@@ -49,54 +49,72 @@ const mass = {
 };
 
 
+
 class App extends React.Component {
 
-  /*constructor() {
-    super();
-    this.title = title;
-    this.author = author;
-    this.publisher = publisher;
-    this.paperBack = paperBack;
-    this.ISBN = ISBN;
-    this.summary = summary;
-    this.img = img;
-    this.stars = 0;
 
-    this.state = {
-      title: 'Max',
-      author: 'asdf',
-      publisher : 'dasdas',
-      paperBack : 'sadas',
-      ISBN : '',
-      summary : '',
-      img : '',
-      stars : ''
-    };
-  }*/
+  renderTabs() {
+    return (
+      <div className="mainTab">
+        <button className="addTab">ADD A BOOK</button>
+        <div className="tab">
+          <button>Now Reading</button>
+          <button>Browse</button>
+          <button>Buy Books</button>
+          <button>Favourite Books</button>
+          <button>Wishlist</button>
+        </div>
+      </div>
+    )
+  }
 
+  renderHeader() {
+    return (
+      <div className="mainHead">
+        <div className="header">
+          <h2>Browse Available Books</h2>
+        </div>
+        <div className="trigger">
+          <button>All books</button>
+          <button>Most Recent</button>
+          <button>Most Popular</button>
+          <button>Free Books</button>
+        </div>
+      </div>
+    )
+  }
+
+  renderBooks() {
+    return (
+      <div className="main">
+        {this.renderTabs()}
+        <div className="app">
+          {this.renderHeader()}
+          <div className="list">
+            {
+              mass.books.map((book) =>
+                <div className="book">
+                  <div className="book-img">
+                    <img
+                      src={book.img}
+                      alt={book.title}
+                    />
+                  </div>
+                  <h3>{book.title}</h3>
+                  <p>{book.author}</p>
+                  <p>{book.stars}</p>
+                </div>
+              )
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   render() {
     return (
-        <div className="list">{
-          mass.books.map((book) =>
-            <div className="book">
-              <div className="book-img">
-                <img
-                  src={book.img}
-                  alt={book.title}
-                />
-              </div>
-              <h3>{book.title}</h3>
-              <p>{book.author}</p>
-              <p>{book.publisher}</p>
-              <p>{book.paperBack}</p>
-              <p>{book.ISBN}</p>
-              <p>{book.summary}</p>
-              <p>{book.stars}</p>
-            </div>
-          )
-        }
-        </div>
+      this.renderBooks()
     )
   }
 }
